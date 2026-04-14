daab8ea5feba1116ea6669450a482f97b6a710e8
eks0684-hue <eks0684@gmail.com>
2026-04-14 19:15:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,26 @@
 const CACHE_NAME = 'kvadratny-anchor-v1';
 
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// INSTALL
+// Установка
 self.addEventListener('install', event => {
-  self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .catch(err => console.error('Cache failed:', err))
   );
 });
 
-// ACTIVATE
+// Активация
 self.addEventListener('activate', event => {
   event.waitUntil(
-    clients.claim(),
     caches.keys().then(keys => {
       return Promise.all(
         keys.map(key => {
@@ -37,7 +33,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// FETCH
+// Запросы (оффлайн режим)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
